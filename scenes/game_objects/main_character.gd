a0477a07a5ede248df934d70e56f7d0be7525910
Hana Zait <hana.o.zait@gmail.com>
2025-10-18 15:32:22 -0700

    Add double jump logic

--- scenes/game_objects/main_character.gd
+++ scenes/game_objects/main_character.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 320.0
 const JUMP_VELOCITY = -900.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
-
+var jump_count = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -18,10 +18,15 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		sprite_2d.animation = "jumping"
+		
+	# Reset jump count when player on floor
+	if is_on_floor():
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
