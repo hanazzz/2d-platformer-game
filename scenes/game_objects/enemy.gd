f43c882faba249821b715cf6858676a0f2d9e924
Hana Zait <hana.o.zait@gmail.com>
2025-10-22 00:16:04 -0700

    Add animation: enemy getting hit

--- scenes/game_objects/enemy.gd
+++ scenes/game_objects/enemy.gd
@@ -1,6 +1,10 @@
 extends RigidBody2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+#enum States {IDLE, HIT}
+#var state : States = States.IDLE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,12 +22,16 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		# Check for relative position of enemy and player
 		var y_delta = position.y - body.position.y
 		var x_delta = body.position.x - position.x
+		# TODO: Separate enemy taking damage and player taking damage into separate functions?
 		# If player touches enemy from above, destroy enemy
 		if y_delta > 30:
-			# Remove enemy node
-			queue_free()
+			# Play hit animation
+			animated_sprite_2d.animation = "hit"
 			# Make player bounce
 			body.jump()
+			# Wait until animation finishes before removing node
+			await animated_sprite_2d.animation_finished
+			queue_free()
 		# If player touches enemy from anywhere else, 
 		# player takes damage amd gets flung away
 		else:
